84828044f60381cca6eb22c300586efa97c46760
heliherrera <heli.herrera.dev@gmail.com>
2021-06-22 22:50:47 -0300

    first commit, simple pascal pomodoro

---
+++ unit1.pas
@@ -0,0 +1,50 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  BCRadialProgressBar;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BCRadialProgressBar1: TBCRadialProgressBar;
+    Button1: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Timer1: TTimer;
+    procedure Label2Click(Sender: TObject);
+    procedure Label3Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Label3Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Label2Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
